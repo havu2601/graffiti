d7c4cc16260945ba9062921fe2d69a6deda7970e
havu2601 <havu2601@172.16.1.87>
2020-05-06 17:27:28 +0700

    template

--- graffiti/src/main/java/filter/LoginFilter.java
+++ graffiti/src/main/java/filter/LoginFilter.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpSession;
  *
  * @author havu2601
  */
-@WebFilter(filterName = "LoginFilter", urlPatterns = {"/index.html"})
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/index.xhtml"})
 public class LoginFilter implements Filter {
     
     @Override
