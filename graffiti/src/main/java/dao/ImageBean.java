2ebb710634d6cf1ac59f8a478ad45073a1d66d53
DELL <DELL@DESKTOP-ASLPDBO>
2020-05-06 22:32:07 +0700

    vy

---
+++ graffiti/src/main/java/dao/ImageBean.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import ejb.ImageEJB;
+import ejb.ProductEJB;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.servlet.http.Part;
+import model.Image;
+import model.Product;
+
+/**
+ *
+ * @author DELL
+ */
+@Named(value = "imageBean")
+@ViewScoped
+public class ImageBean implements Serializable{
+
+    @EJB
+    private ImageEJB ejbImage;
+    
+    @EJB
+    private ProductEJB ejbProduct;
+    
+    List<Image> listImage;
+    Product objProduct;
+    Image objImage;
+    String productId;
+    Part image;
+    
+    @PostConstruct
+    public void init(){
+        objProduct = new Product();
+        listImage = new ArrayList<>();
+    }
+
+    public void search(){
+        objProduct = ejbProduct.findById(Integer.parseInt(productId));
+        listImage = ejbImage.findByProduct(objProduct.getProductId());
+    }
+           
+    public void addToList(){
+        doUpload();
+        objImage = new Image();
+        objImage.setImagePath("../assets/images/" + image.getSubmittedFileName());
+        objImage.setProductId(objProduct);
+        ejbImage.addImage(objImage);
+        listImage = new ArrayList<>();
+        listImage = ejbImage.findByProduct(objProduct.getProductId());
+        objImage = new Image();
+    }
+
+    public void delete(int id){
+        objImage = new Image();
+        objImage = ejbImage.findById(id);
+        listImage.remove(objImage);
+        ejbImage.delete(objImage);
+        objImage = new Image();
+    }
+    
+    public void doUpload(){
+        try {
+            InputStream in = image.getInputStream();
+            
+            File f = new File("D:\\PRJ4\\gitCode\\graffiti\\graffiti\\src\\main\\webapp\\assets\\images\\" + image.getSubmittedFileName());
+            f.createNewFile();
+            FileOutputStream out = new FileOutputStream(f);
+            
+            byte[] buffer = new byte[1024];
+            int length;
+            
+            while((length= in.read(buffer))>0){
+                out.write(buffer, 0, length);
+            }
+            
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public List<Image> getListImage() {
+        return listImage;
+    }
+
+    public void setListImage(List<Image> listImage) {
+        this.listImage = listImage;
+    }
+
+    public Image getObjImage() {
+        return objImage;
+    }
+
+    public void setObjImage(Image objImage) {
+        this.objImage = objImage;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Part getImage() {
+        return image;
+    }
+
+    public void setImage(Part image) {
+        this.image = image;
+    }
+
+    public Product getObjProduct() {
+        return objProduct;
+    }
+
+    public void setObjProduct(Product objProduct) {
+        this.objProduct = objProduct;
+    }
+    
+    
+}
--- graffiti/src/main/java/dao/ProductBean.java
+++ graffiti/src/main/java/dao/ProductBean.java
@@ -12,10 +12,16 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.http.Part;
+import model.Brand;
+import model.Color;
 import model.Image;
 import model.Product;
+import model.SubCategory;
+
+
 
 /**
  *
@@ -36,6 +42,17 @@ public class ProductBean implements Serializable{
     
     Product objProduct;
     Image objImage;
+    Brand objBrand;
+    SubCategory objSubCat;
+    Color objColor;
+    
+    String brandId;
+    String subcatId;
+    String colorId;
+    String capacity;
+    String stock;
+    String price;
+    Part image;
     
     String searchStr;
     String searchType;
@@ -62,13 +79,68 @@ public class ProductBean implements Serializable{
         }
     }
 
-    public void loadProduct(){
-        
+    public void loadProduct(int id){
+        objProduct = ejbProduct.findById(id);
+        brandId = objProduct.getBrandId().getBrandId().toString();
+        subcatId = objProduct.getSubcatId().getSubcatId().toString();
+        colorId = objProduct.getColorId().getColorId().toString();
+        capacity = String.valueOf(objProduct.getProductCapacity());
+        price = String.valueOf(objProduct.getProductPrice());
+        stock = String.valueOf(objProduct.getProductStock());
     }
     
     public void reset(){
         objProduct = new Product();
         listImage = new ArrayList<>();
+        brandId = "";
+        subcatId = "";
+        colorId = "";
+        capacity = "";
+        price = "";
+        stock = "";
+    }
+    
+    public String addNewProduct(){
+        if(null==objProduct.getProductId()){
+            updateProduct();
+            ejbProduct.addProduct(objProduct);
+        }else{
+            updateProduct();
+            ejbProduct.updateProduct(objProduct);
+            reset();
+        }
+        
+        return "product.xhtml";
+    }
+    
+    public void updateProduct(){
+        objProduct.setProductPrice(Double.parseDouble(price));
+        objProduct.setProductCapacity(Integer.parseInt(capacity));
+        objProduct.setProductStock(Integer.parseInt(stock));
+        updateBrand();
+        objProduct.setBrandId(objBrand);
+        updateColor();
+        objProduct.setColorId(objColor);
+        updateSubCat();
+        objProduct.setSubcatId(objSubCat);
+    }
+    public void updateBrand(){
+        objBrand = new Brand();
+        objBrand.setBrandId(Integer.parseInt(brandId));
+    }
+    
+    public void updateSubCat(){
+        objSubCat = new SubCategory();
+        objSubCat.setSubcatId(Integer.parseInt(subcatId));
+    }
+    
+    public void updateColor(){
+        objColor = new Color();
+        objColor.setColorId(Integer.parseInt(colorId));
+    }
+
+    public String moveToImage(int id){
+        return "image.xhtml?pid="+id+"&faces-redirect=true";
     }
     
     public List<Product> getListProduct() {
@@ -103,6 +175,78 @@ public class ProductBean implements Serializable{
         this.objImage = objImage;
     }
 
+    public Brand getObjBrand() {
+        return objBrand;
+    }
+
+    public void setObjBrand(Brand objBrand) {
+        this.objBrand = objBrand;
+    }
+
+    public SubCategory getObjSubCat() {
+        return objSubCat;
+    }
+
+    public void setObjSubCat(SubCategory objSubCat) {
+        this.objSubCat = objSubCat;
+    }
+
+    public Color getObjColor() {
+        return objColor;
+    }
+
+    public void setObjColor(Color objColor) {
+        this.objColor = objColor;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getSubcatId() {
+        return subcatId;
+    }
+
+    public void setSubcatId(String subcatId) {
+        this.subcatId = subcatId;
+    }
+
+    public String getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(String colorId) {
+        this.colorId = colorId;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
     public String getSearchStr() {
         return searchStr;
     }
@@ -119,6 +263,15 @@ public class ProductBean implements Serializable{
         this.searchType = searchType;
     }
     
+    //IMAGE
+
+    public Part getImage() {
+        return image;
+    }
+
+    public void setImage(Part image) {
+        this.image = image;
+    }
     
     
 }
